d3d40883d42021b027e0a22e6d1925d7c950bfe4
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-03-16 10:58:30 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(2, 4);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(value){
+    return function(fare) {
+        return fare * value;
+    }
+}
+function fareDoubler(fare) {
+    return fare + fare;
+}
+function fareTripler(fare) {
+    return fare * 3;
+}
+function selectDifferentDrivers(drivers,returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers)
+}
